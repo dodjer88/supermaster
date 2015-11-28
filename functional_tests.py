9c7d58980b83cabbc6747056516f3bd4d9dfb4c4
dodjer88 <dodjer88@gmail.com>
2015-11-28 13:04:52 +0200

    create unittest and modif first test

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,26 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_page(self):
+
+        self.browser.get('http://localhost:8000')
+        self.assertIn('nogotki',self.browser.title)
+        self.fail('Finish the test')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+
 
